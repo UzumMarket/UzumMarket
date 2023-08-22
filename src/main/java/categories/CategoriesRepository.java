0e2c4fc2584d4c96ec201cc422fdf81c57bc7ee4
user <odilxonabdullayev9@gmail.com>
2023-08-22 14:12:29 +0300

    Repository bo'ldi

---
+++ src/main/java/categories/CategoriesRepository.java
@@ -0,0 +1,22 @@
+package categories;
+
+import common.BaseRepository;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CategoriesRepository extends BaseRepository<UUID, Categories> {
+    private static final CategoriesRepository categoriesRepository = new CategoriesRepository();
+
+    @Override
+    protected String getFileName() {
+        return "src/resources/categories.txt";
+    }
+
+    public static CategoriesRepository getInstance() {
+        return categoriesRepository;
+    }
+
+}
