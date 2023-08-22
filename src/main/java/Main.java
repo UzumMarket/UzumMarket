100d898b396793fbbd60f9b37af8bbf422f9dee6
Xojiakbar Kamolov <xdasturchi777@gmail.com>
2023-08-22 18:55:20 +0500

    Changed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,5 @@
 public class Main {
+    public static void main(String[] args) {
 
-     public static void main(String[] args) {
-
-         System.out.println("Barchasi Togirlandi iltimos extiyot bulib ishlating" +
-                 "agar ochirib yuborsangiz uziz qilib chiqasiz");
-    }
-
-    private void s(){
-        System.out.println("s");
     }
 }
