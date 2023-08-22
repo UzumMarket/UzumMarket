d68e78262986cc9da6b653ccc88ac019b64e0590
Xojiakbar Kamolov <xdasturchi777@gmail.com>
2023-08-22 14:00:47 +0500

    Changedssregrh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Ss");
-        System.out.println("Togirladim");
+
+     public static void main(String[] args) {
+
     }
 }
