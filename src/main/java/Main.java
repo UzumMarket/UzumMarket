4b960d0a021b310c99229bbb308510d133f64ac6
user <odilxonabdullayev9@gmail.com>
2023-08-22 17:32:46 +0300

    Push10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
          System.out.println("Barchasi Togirlandi iltimos extiyot bulib ishlating" +
                  "agar ochirib yuborsangiz uziz qilib chiqasiz");
-         System.out.println("Abdefghijklmnopqrstuvxyzogshch");
+         System.out.println("🤬Push10");
     }
 }
