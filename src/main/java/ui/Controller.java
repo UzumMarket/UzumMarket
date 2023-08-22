3d2de75715b6b72a7b07b8ef19865aea1b05cede
Xojiakbar Kamolov <xdasturchi777@gmail.com>
2023-08-22 20:08:25 +0500

    UIlarga Servicelar qoshildi!

--- src/main/java/ui/Controller.java
+++ src/main/java/ui/Controller.java
@@ -1,6 +1,14 @@
 package ui;
 
+import categories.CategoriesService;
+import mail.Gmail;
+import product.ProductService;
+import user.UserService;
+
 public class Controller implements UI {
+
+    private final UserService userService = UserService.getInstance();
+    private final Gmail gmail = Gmail.getInstance();
 
     @Override
     public void start() {
--- src/main/java/ui/UserUI.java
+++ src/main/java/ui/UserUI.java
@@ -1,6 +1,17 @@
 package ui;
 
-public class UserUI implements UI{
+import categories.Categories;
+import categories.CategoriesService;
+import mail.Gmail;
+import product.ProductService;
+import user.UserService;
+
+public class UserUI implements UI {
+
+    private final UserService userService = UserService.getInstance();
+    private final CategoriesService categoriesService = CategoriesService.getInstance();
+    private final ProductService productService = ProductService.getInstance();
+
     @Override
     public void start() {
 
