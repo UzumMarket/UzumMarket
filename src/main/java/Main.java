dfe3e2551577daea0e146bcfc26e7d6a9ad08b56
user <odilxonabdullayev9@gmail.com>
2023-08-23 13:19:30 +0300

    File Handler

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import logger.FileHandlers;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger logger = Logger.getGlobal();
+
+    static {
+        logger.setLevel(Level.ALL);
+        logger.addHandler(new FileHandlers());
+        logger.setUseParentHandlers(false);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Barchasi Togirlandi iltimos extiyot bulib ishlating" +
+                "agar ochirib yuborsangiz uziz qilib chiqasiz");
+        logger.log(Level.INFO, "Hello world");
+    }
+
+    private void s() {
+        System.out.println("s");
     }
 }
